d100eeff85a1d2babcd58139d75a28f3b9e5a2b6
Emilio Haro <ermsharo@gmail.com>
2023-06-01 19:13:29 -0300

    update our changes

--- TerceiraEntrega/Backend/main.py
+++ TerceiraEntrega/Backend/main.py
@@ -2,13 +2,24 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 # Create a Flask application
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://username:password@localhost/db_name'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///BD_2.db'
 db = SQLAlchemy(app)
+
+# Lets add the nations
+
+def add_nation():
+    print("add nation")
+
 # Define a route and its corresponding handler
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
+
+def main():
+    print('running main')
+    app.run()
+
 # Run the Flask application
 if __name__ == '__main__':
-    app.run()
+    main()
