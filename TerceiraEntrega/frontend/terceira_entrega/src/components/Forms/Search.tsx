a86fa7c999b71aed32a545215fae3b4b265665a3
Emilio Haro <ermsharo@gmail.com>
2023-06-01 15:57:29 -0300

    add changes

--- TerceiraEntrega/frontend/terceira_entrega/src/components/Forms/Search.tsx
+++ TerceiraEntrega/frontend/terceira_entrega/src/components/Forms/Search.tsx
@@ -3,49 +3,38 @@ import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 
 function Bar() {
-    return (
-      <Box
-        sx={{
-          height: 24,
-        }}
-      />
-    );
-  }
+  return (
+    <Box
+      sx={{
+        height: 24,
+      }}
+    />
+  );
+}
 
 export default function Search() {
   return (
     <Box component="form" noValidate autoComplete="off">
       <div>
-      <Bar/>
+        <Bar />
+        <TextField fullWidth id="outlined" label="Jogador" />
+        <Bar />
+        <TextField fullWidth id="outlined" label="Arbitro" />
+        <Bar />
+        <TextField
+          fullWidth
+          id="outlined"
+          label="Local"
+          defaultValue="Hello World"
+        />
+        <Bar />
         <TextField
           fullWidth
           id="outlined"
           label="Required"
           defaultValue="Hello World"
         />
-        <Bar/>
-        <TextField
-          fullWidth
-          id="outlined"
-          label="Required"
-          defaultValue="Hello World"
-        />
-        <Bar/>
-        <TextField
-          fullWidth
-          id="outlined"
-          label="Required"
-          defaultValue="Hello World"
-        />
-        <Bar/>
-        <TextField
-          fullWidth
-          id="outlined"
-          label="Required"
-          defaultValue="Hello World"
-        />
-        <Bar/> 
-
+        <Bar />
       </div>
     </Box>
   );
