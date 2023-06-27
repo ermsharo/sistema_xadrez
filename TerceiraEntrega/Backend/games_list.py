32bb6dc702672f78a1ef476f27c65aba9e7e7c35
Emilio Haro <ermsharo@gmail.com>
2023-06-27 13:50:50 -0300

    update or changes

--- TerceiraEntrega/Backend/games_list.py
+++ TerceiraEntrega/Backend/games_list.py
@@ -1,8 +1,23 @@
 from flask import Blueprint
+from models import (
+ create_partida_model
+)
 
 games_routes = Blueprint('parameters', __name__)
 
-@games_routes.route('/hotel')
+
+def pegar_partidas():
+    Partidas = create_partida_model()
+    partidas_records = Partidas.query.all()
+    # print("Campeonato", Paises)
+    # record_list = [partidas.__dict__ for partidas in partidas_records]
+    # #print(" \n Record_list", record_list)
+    # #print(" \n Record_list type:", type(record_list))
+
+    # new_record_list = [{"id": d["id"], "nome": d["nome"]} for d in record_list]
+    return partidas_records
+
+@games_routes.route('/games')
 def hotel_route():
     # Create your object
     
--- TerceiraEntrega/Backend/main.py
+++ TerceiraEntrega/Backend/main.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import and_
 from models import (
     create_jogadas_model,
     create_participantes_campeonato_model,
@@ -21,6 +22,7 @@ from flask_cors import CORS
 from random import randint, random
 from datetime import datetime, timedelta
 import random
+from sqlalchemy.orm import joinedload
 
 # Create a Flask application
 app = Flask(__name__)
@@ -63,7 +65,7 @@ def generate_rand_hospedagem():
     random_id = random_query.id
     print("Rand_id", random_id)
     print("Rand_id", random_query.nome)
-    return {'id': random_id, 'nome': random_query.nome}
+    return {"id": random_id, "nome": random_query.nome}
 
 
 def subcribe_player_hotel(champeonato_id, start, end, hospedagem_id, participante):
@@ -106,7 +108,9 @@ def subcribe_salao(n, start_date, end_date):
     return intervals
 
 
-def simulate_championship(players, arbrito, chave_campeonato, start_date, end_date, rand_hospedagem):
+def simulate_championship(
+    players, arbrito, chave_campeonato, start_date, end_date, rand_hospedagem
+):
     num_players = len(players)
     num_games = 4
 
@@ -132,8 +136,6 @@ def simulate_championship(players, arbrito, chave_campeonato, start_date, end_da
         "numero_jogadas",
     ]
 
-
-
     with open("championship.csv", "w", newline="") as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
@@ -149,10 +151,12 @@ def simulate_championship(players, arbrito, chave_campeonato, start_date, end_da
             data_inicio = game_time["start_moment"]
             data_fim = game_time["end_moment"]
             vencedor = jogador_primario if randint(0, 1) == 0 else jogador_secundario
-            hosp_id = rand_hospedagem['id']
-            hosp_name = rand_hospedagem['nome']
+            hosp_id = rand_hospedagem["id"]
+            hosp_name = rand_hospedagem["nome"]
             print("Hosp_id", hosp_id)
-            chave_salao =  subscribe_game_place(hosp_id,hosp_name, chave_campeonato, data_inicio, data_fim)
+            chave_salao = subscribe_game_place(
+                hosp_id, hosp_name, chave_campeonato, data_inicio, data_fim
+            )
             numeros_jogadas = randint(6, 35)
             writer.writerow(
                 {
@@ -171,7 +175,19 @@ def simulate_championship(players, arbrito, chave_campeonato, start_date, end_da
                 }
             )
 
-            subscribe_game(jogador_primario,jogador_secundario,arbitro,jogador_primario,jogador_secundario,data_inicio,data_fim,vencedor,chave_campeonato,chave_salao,numeros_jogadas)
+            subscribe_game(
+                jogador_primario,
+                jogador_secundario,
+                arbitro,
+                jogador_primario,
+                jogador_secundario,
+                data_inicio,
+                data_fim,
+                vencedor,
+                chave_campeonato,
+                chave_salao,
+                numeros_jogadas,
+            )
 
     print(
         f"Championship with {num_games} games simulated and saved to championship.csv."
@@ -181,11 +197,14 @@ def simulate_championship(players, arbrito, chave_campeonato, start_date, end_da
 def pegar_campeonatos():
     Campeonato = create_campeonato_model()
     Campeonato_records = Campeonato.query.all()
-    #print("Campeonato", Campeonato_records)
+    # print("Campeonato", Campeonato_records)
     record_list = [campeonato.__dict__ for campeonato in Campeonato_records]
     print("Aqui esta", record_list)
-    new_record_list = [{'id': d['id'], 'nome': d['nome'], 'data_inicio': d['data_inicio'] } for d in record_list]
-    print(" \n \n  new rocrd list",new_record_list)
+    new_record_list = [
+        {"id": d["id"], "nome": d["nome"], "data_inicio": d["data_inicio"]}
+        for d in record_list
+    ]
+    print(" \n \n  new rocrd list", new_record_list)
     return new_record_list
 
 
@@ -267,19 +286,21 @@ def get_matches(campeonato_id, start_date):
         randPlayersList.append(randp)
         print(" \n \n rand p id", randp)
         subscribe_championship(randp, "PLAYER", campeonato_id, nacao)
-        subscribe_player_host(campeonato_id,start, end, rand_hospedagem['id'], randp)
+        subscribe_player_host(campeonato_id, start, end, rand_hospedagem["id"], randp)
 
     # Pegamos um arbitro deste pais
     randJudge = get_rand_judge_by_nation(arbitro)
     subscribe_championship(randJudge, "JUDGE", campeonato_id, arbitro)
-    subscribe_player_host(campeonato_id,start, end, rand_hospedagem['id'], randJudge)
+    subscribe_player_host(campeonato_id, start, end, rand_hospedagem["id"], randJudge)
     # Cadastramos essas pessoas no campeonato
     player_list = randPlayersList
     arbrito = randJudge
     chave_campeonato = campeonato_id
 
     # Gerar os jogos
-    simulate_championship(player_list, arbrito, chave_campeonato, start, end,rand_hospedagem)
+    simulate_championship(
+        player_list, arbrito, chave_campeonato, start, end, rand_hospedagem
+    )
     # pegar_campeonatos(player_list,arbrito,chave_campeonato)
 
 
@@ -291,10 +312,10 @@ def montar_chaves_campeonato():
         print("\n ", campeonato["id"])
 
 
-def subscribe_game_place(host_place,host_name, championship, start, end):
+def subscribe_game_place(host_place, host_name, championship, start, end):
     Registro_salao = create_salao_model()
     posibles_names = ["Big room A", "Big room B", "Big room C", "Big room D"]
-    place_name = f'{host_name}  -  {random.choice(posibles_names)}'
+    place_name = f"{host_name}  -  {random.choice(posibles_names)}"
 
     novo_registro_salao = Registro_salao(
         capacidade=random.randrange(5, 101),
@@ -306,14 +327,13 @@ def subscribe_game_place(host_place,host_name, championship, start, end):
         chave_campeonato=championship,
         inicio_uso=start,
         fim_uso=end,
-        chave_hospedagem = host_place,
+        chave_hospedagem=host_place,
     )
     db.session.add(novo_registro_salao)
     db.session.commit()
 
     new_record_id = novo_registro_salao.id
     return new_record_id
-
 
 
 def subscribe_player_host(championship, start, end, place, player):
@@ -333,21 +353,33 @@ def subscribe_player_host(championship, start, end, place, player):
     return new_record_id
 
 
-def subscribe_game(jogador_primario,jogador_secundario,arbitro,pecas_pretas,pecas_brancas,data_inicio,data_fim,vencedor,chave_campeonato,chave_salao,numero_jogadas):
+def subscribe_game(
+    jogador_primario,
+    jogador_secundario,
+    arbitro,
+    pecas_pretas,
+    pecas_brancas,
+    data_inicio,
+    data_fim,
+    vencedor,
+    chave_campeonato,
+    chave_salao,
+    numero_jogadas,
+):
     Registro_partida = create_partida_model()
 
     novo_registro_partida = Registro_partida(
-                    jogador_primario =  jogador_primario,
-                    jogador_secundario = jogador_secundario,
-                    arbitro =  arbitro,
-                    pecas_pretas =  pecas_brancas,
-                    pecas_brancas =  pecas_pretas,
-                    data_inicio =  data_inicio,
-                    data_fim =  data_fim,
-                    vencedor =  vencedor,
-                    chave_campeonato =  chave_campeonato,
-                    chave_salao =  chave_salao,
-                    numero_jogadas =numero_jogadas,
+        jogador_primario=jogador_primario,
+        jogador_secundario=jogador_secundario,
+        arbitro=arbitro,
+        pecas_pretas=pecas_brancas,
+        pecas_brancas=pecas_pretas,
+        data_inicio=data_inicio,
+        data_fim=data_fim,
+        vencedor=vencedor,
+        chave_campeonato=chave_campeonato,
+        chave_salao=chave_salao,
+        numero_jogadas=numero_jogadas,
     )
     db.session.add(novo_registro_partida)
     db.session.commit()
@@ -356,7 +388,56 @@ def subscribe_game(jogador_primario,jogador_secundario,arbitro,pecas_pretas,peca
     return new_record_id
 
 
+def pegar_partidas():
+    Partida = create_partida_model()
+    partidas_records = Partida.query.all()
+    participantes = pegar_participantes()
+    record_list = [partida.__dict__ for partida in partidas_records]
+    new_record_list = [
+        {
+            "jogador_primario": d["jogador_primario"],
+            "jogador_secundario": d["jogador_secundario"],
+            "jogador_primario_nome": query_participantes(participantes,d["jogador_primario"]),
+            "jogador_secundario_nome": query_participantes(participantes,d["jogador_secundario"]),
+            "arbitro": d["arbitro"],
+            "pecas_pretas": d["pecas_pretas"],
+            "pecas_brancas": d["pecas_brancas"],
+            "data_inicio": d["data_inicio"],
+            "data_fim": d["data_fim"],
+            "vencedor": d["vencedor"],
+            "chave_campeonato": d["chave_campeonato"],
+            "chave_salao": d["chave_salao"],
+            "numero_jogadas": d["numero_jogadas"],
+        }
+        for d in record_list
+    ]
+    print("Partida records:", new_record_list)
 
+   
+
+
+def pegar_participantes():
+    Participantes = create_participantes_model()
+    participantes_records = Participantes.query.all()
+    print("Partida records:", participantes_records)
+    record_list = [participantes.__dict__ for participantes in participantes_records]
+    new_record_list = [{"id": d["id"], "nome": d["nome"]} for d in record_list]
+    print("new record list", new_record_list)
+    return new_record_list
+
+
+def query_participantes(participantes, id):
+    for record in participantes:
+        if record["id"] == id:
+            return record["nome"]
+    return None
+
+
+def query_pais(pais, id):
+    for record in pais:
+        if record["id"] == id:
+            return record["nome"]
+    return None
 
 def main():
     # Usage example:
@@ -364,13 +445,17 @@ def main():
         # add_based_in_csv_hospedagem()
         # for nacao in nacoes:
         #     print(nacao.nome)
-        #get_matches(2, "2023-06-06")
-        campeonatos = pegar_campeonatos()
-        for campeonato in campeonatos:
-            print("\n ", campeonato["nome"])
-            print("\n ", campeonato["id"])
-            print("\n ", campeonato["data_inicio"])
-            get_matches(campeonato["id"],campeonato["data_inicio"])
+        # get_matches(2, "2023-06-06")
+        # campeonatos = pegar_campeonatos()
+        # for campeonato in campeonatos:
+        #     print("\n ", campeonato["nome"])
+        #     print("\n ", campeonato["id"])
+        #     print("\n ", campeonato["data_inicio"])
+        #     get_matches(campeonato["id"],campeonato["data_inicio"])
+
+        pegar_partidas()
+       
+
     app.run()
 
 
